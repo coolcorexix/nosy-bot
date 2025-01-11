1cb8f32ce7216a9adf2aab9a3317a1106f3bcb8c
Phat Intern <coolcore.xix@gmail.com>
2025-01-11 18:03:22 +0700

    deploy script

--- restart_prod.sh
+++ restart_prod.sh
@@ -1,4 +1,4 @@
 git pull --ff 
 path/to/venv/bin/pip3 install -r requirements.txt
 path/to/venv/bin/python3 migrations/run_migrations.py up
-path/to/venv/bin/python3 runbot.py
+path/to/venv/bin/python3 run_bot.py
